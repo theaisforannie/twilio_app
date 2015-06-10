bdb999b5ce7372dae7d1ed761da05c3ea1795d60
theaisforannie <annharter@gmail.com>
2015-06-10 18:05:44 -0400

    omgomgomgomgomgomgomg flatterybot via text!! added compliments and changed sms-quickstart to reflect omgomgomgomg

--- sms-quickstart.rb
+++ sms-quickstart.rb
@@ -1,13 +1,13 @@
 require 'rubygems'
 require 'twilio-ruby'
 require 'sinatra'
+require 'yaml'
+
+compliments = YAML.load_file("compliments.yml")
 
 get '/sms-quickstart' do
 	twiml = Twilio::TwiML::Response.new do |r|
-		r.Message do |message|
-			message.Body "Body"
-			message.MediaUrl "http://demo.twilio.com/owl.png"
-		end
+		r.Message compliments.sample
 	end
 	twiml.text
 end
